9184b38e136703f92fcfdc372ac4bcba481e3f99
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 08:45:56 +0900

    3. 변수 선언과 초기화, 활용

--- lecture03.js
+++ lecture03.js
@@ -7,3 +7,13 @@ a = 10; // a 변수 10으로 초기화
 
 var a = 10; // 변수 선언과 동시에 초기화
 var a=10, b=20, c=30; // 한 번에 여러변수 선언 동시에 초기화
+
+
+// 변수의 활용
+// var msg;
+// msg = "Hello World";
+// // alert(msg);
+// console.log(msg);
+var name = prompt("이름을 입력해주세요."); // prompt 명령어를 통해 사용자 이름 입력 받기
+console.log(name, "님 환영합니다.") // console.log를 통해 출력하기
+
