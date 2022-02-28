fde40ac9d0878e915665b3a397d6afb11c1f392d
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 08:19:22 +0900

    2. developer console 사용하기

---
+++ lecture02.js
@@ -0,0 +1,13 @@
+console.log("Hello developer console");
+console.log("2");
+console.log("3");
+// javascript는 브라우저에서 한 줄씩 실행된다.
+// 책을 읽는 것과 비슷하다. 
+// 한 줄을 읽고 해당 명령을 실행하는 방식.
+
+console.log("print from console");
+alert("hello developer console");
+
+
+
+
