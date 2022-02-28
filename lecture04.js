8589f2c6f51138410e22bd3cdda49dff74797b73
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 08:59:57 +0900

    4. 기본자료형(number, string, boolean), typeof()

---
+++ lecture04.js
@@ -0,0 +1,13 @@
+// 자료형이란, 변수에 저장할 수 있는 값의 종류(Type)
+
+// 기본자료형
+var a=100, b=3.14; // 숫자 자료형 변수 선언, 초기화(number type)
+var c="안녕하세요", d="a"; // 문자열 자료형 변수 선언, 초기화(string type)
+var e=true, f=false; // boolean 자료형 변수 선언, 초기화(boolean type) / (참, 거짓을 나타내는 자료형)
+
+// typeof() : 괄호 안에 들어있는 변수가 어떤 자료형인지 (console 창에서) 알려주는 명령어
+console.log(a, typeof(a));
+console.log(c, typeof(c));
+console.log(e, typeof(e));
+
+
