b84d63a81ed4b50004dee27e4ba9efcdeecd5da6
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 19:48:52 +0900

    *. 배열 사용하기(pop, push, shift, unshift, sort, split)

---
+++ array02.js
@@ -0,0 +1,24 @@
+// 배열 사용하기
+
+// pop, push, shift, unshift
+// reverse() - 뒤집어진 배열 반환(거꾸로)
+// sort() - 배열 값 정렬해서 반환
+// split() - 배열을 반환하는 문자열 명령어 (",")
+
+// <배열에 엘리먼트 추가/삭제하기>
+// 기본적으로 배열의 앞과 뒤에서 엘리먼트를 추가하거나 삭제할 수 있음.
+
+// - .push(element) : 배열의 뒤에 엘리먼트 추가
+// - .pop() : 배열의 뒤에서 엘리먼트 삭제하고 리턴
+// - .shift() : 배열의 앞에서 엘리먼트 삭제하고 리턴
+// - .unshift(element) : 배열의 앞에 엘리먼트 추가
+
+
+// <배열 붙이기, 검색하기>
+// - arr1.concat(arr2) : arr1과 arr2 붙임
+// - arr.indexOf(element) : arr에서 element가 있는 첫 위치를 검색
+// - arr.lastIndexOf(element) : arr에서 element가 있는 마지막 위치를 검색
+
+
+// <문자열 split 함수>
+// - 문자열을 구분자(separator)로 나눠서 각각을 담은 배열을 반환하는 함수
