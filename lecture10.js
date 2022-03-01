c403c535b8973ff5a892c0e9d5c75a40e5bffc55
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 18:44:21 +0900

    10. 관계 연산자와 논리 연산자, and, or, not 연산자

---
+++ lecture10.js
@@ -0,0 +1,23 @@
+// 관계연산자와 논리연산자
+
+// 관계 연산자
+// Relational Operator
+// 피연산자 a, 피연산자 b 비교
+// 두 표현식(A, B)의 관계를 비교하는 이항연산자.
+// 관계에 따라 boolean 타입의 true, false로 표현됨.
+
+var a=1, b=2, c=3, d=4;
+var e="a", f="b", g="c", h="d";
+
+// 논리 연산자
+// Logical Operator
+// 두 boolean 피연산자에 대해 연산하는 연산자
+
+// AND(&&) 연산자
+// 두 피연산자가 모두 true인 경우에만 true를 나타내는 이항연산자
+
+// OR(||) 연산자
+// 두 피연산자가 하나라도 true인 경우 true를 나타내는 이항연산자
+
+// NOT(!) 연산자
+// 피연산자가 true인 경우 false를, false인 경우 true를 나타내는 단항연산자
