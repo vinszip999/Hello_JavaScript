966915578586c228abd625f5121d76ed3cbc7690
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 19:19:08 +0900

    *. String 다루기(charAt, indexOf, lastIndexOf)

---
+++ string02.js
@@ -0,0 +1,30 @@
+// String 다루기
+
+// 문자열.charAt - 문자열 특정 위치 접근 시 사용
+// 문자열.indexOf - 문자열 위치 반환(첫번째)
+// 문자열.lastIndexOf - 문자열 위치 반환(마지막 위치)
+
+// <특정 위치의 문자열 알아내기>
+// - .charAt 함수 이용
+//   - 첫 문자 : str.charAt(0)
+//   - 마지막 문자 : str.charAt(str.length-1)
+// - 대괄호([]) 사용
+//   - 첫 문자 : str[0]
+//   - 마지막 문자 : str[str.length-1]
+
+
+// <부분문자열 구하기>
+// 문자열의 연속된 일부분을 구하는 함수
+
+// - .substring(pos1, pos2) : pos1 에서 pos2까지의 부분 문자열 반환
+//   - pos2 생략시 pos1에서부터 마지막 까지의 문자열 반환
+// - substr(pos, length) : pos에서 length길이 만큼의 부분 문자열 반환
+//   - length 생략시, pos에서 마지막까지의 문자열 반환
+//   - pos 가 음수인 경우, str.length - pos 로 동작
+
+
+// <문자열 검색하기>
+// - indexOf(str) :
+// - lastIndexOf(str) :
+
+// 존재하지 않는 문자열 입력 경우, -1 반환
